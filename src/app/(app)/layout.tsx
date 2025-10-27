b3a08ac3238182452e453e61f408e9d932656b8b
Sai Hareeka <saihareeka31@gmail.com>
2025-10-27 22:31:37 +0000

    https://my-site-c25ybdv6-chithrapriyadarshi.wix-vibe.com/ can you see th

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -166,20 +166,22 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <FirebaseClientProvider>
-      <AuthProvider>
-        <SidebarProvider>
-          <div className="flex min-h-screen w-full">
-            <AppSidebar />
-            <div className="flex flex-col w-full">
-              <AppHeader />
-              <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 bg-muted/40">
-                  {children}
-              </main>
+    <div className="dark">
+      <FirebaseClientProvider>
+        <AuthProvider>
+          <SidebarProvider>
+            <div className="flex min-h-screen w-full">
+              <AppSidebar />
+              <div className="flex flex-col w-full">
+                <AppHeader />
+                <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 bg-background">
+                    {children}
+                </main>
+              </div>
             </div>
-          </div>
-        </SidebarProvider>
-      </AuthProvider>
-    </FirebaseClientProvider>
+          </SidebarProvider>
+        </AuthProvider>
+      </FirebaseClientProvider>
+    </div>
   );
 }
