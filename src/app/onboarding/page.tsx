f99d942ce348a3b3ffae4b1494fcbfaf746051aa
Sai Hareeka <saihareeka31@gmail.com>
2025-10-27 20:34:16 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,7 +13,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
